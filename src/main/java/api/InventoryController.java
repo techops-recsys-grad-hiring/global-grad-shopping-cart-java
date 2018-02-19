2ef2effbd77fa10dd52916c2aad5ffde94a9eebf
Vivian Yamamoto <vivian.yamamoto@thoughtworks.com>
2018-02-19 11:31:17 -0800

    Adds update API [Vivian]

--- src/main/java/api/InventoryController.java
+++ src/main/java/api/InventoryController.java
@@ -2,6 +2,7 @@ package api;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,8 +11,14 @@ public class InventoryController {
     @Autowired
     private GildedRose gildedRose;
 
-    @RequestMapping("/get")
+    @RequestMapping(value = "/get")
     public Item[] getInventory() {
+        return gildedRose.items;
+    }
+
+    @RequestMapping(value = "/update")
+    public Item[] updateInventory() {
+        gildedRose.updateQuality();
         return gildedRose.items;
     }
 }
