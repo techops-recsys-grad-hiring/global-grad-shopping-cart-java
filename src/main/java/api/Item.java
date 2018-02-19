3f2fe684a7e590c7c2762b933e70026491f482d7
Vivian Yamamoto <vivian.yamamoto@thoughtworks.com>
2018-02-19 10:42:59 -0800

    Imports gilded rose kata code as is from https://github.com/emilybache/GildedRose-Refactoring-Kata/blob/master/Java/

---
+++ src/main/java/api/Item.java
@@ -0,0 +1,22 @@
+package api;
+
+
+public class Item {
+
+    public String name;
+
+    public int sellIn;
+
+    public int quality;
+
+    public Item(String name, int sellIn, int quality) {
+        this.name = name;
+        this.sellIn = sellIn;
+        this.quality = quality;
+    }
+
+    @Override
+    public String toString() {
+        return this.name + ", " + this.sellIn + ", " + this.quality;
+    }
+}
