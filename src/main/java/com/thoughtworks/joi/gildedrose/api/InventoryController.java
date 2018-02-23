4b03c2003b19a63f9d93a6d6f10f67e91102c340
Angel Villalain <angel.villalain@gmail.com>
2018-02-23 11:46:25 -0600

    Added CrossOrigin header

--- src/main/java/com/thoughtworks/joi/gildedrose/api/InventoryController.java
+++ src/main/java/com/thoughtworks/joi/gildedrose/api/InventoryController.java
@@ -3,9 +3,11 @@ package com.thoughtworks.joi.gildedrose.api;
 import com.thoughtworks.joi.gildedrose.domain.GildedRose;
 import com.thoughtworks.joi.gildedrose.domain.Item;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/inventory")
 public class InventoryController {
